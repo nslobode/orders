08dcfe2c6eb2fba85bf3a00c6691c0b4cdec6716
Nikolai Slobodeniuk <Nikolai.Slobodeniuk@syngenta.com>
2020-04-21 11:54:24 +0300

    Redo beans injection Extract logic to Runner class

--- src/main/java/org/parser/orders/OrdersApplication.java
+++ src/main/java/org/parser/orders/OrdersApplication.java
@@ -1,37 +1,18 @@
 package org.parser.orders;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.parser.orders.model.Order;
-import org.parser.orders.processor.impl.CsvProcessor;
-import org.parser.orders.processor.impl.JsonProcessor;
-import org.parser.orders.utils.Checker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.parser.orders.runner.Runner;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.Stream;
-
-@Log4j2
 @SpringBootApplication
-@EnableAsync(proxyTargetClass = true)
-@EnableCaching
+@EnableAsync
 @AllArgsConstructor
 public class OrdersApplication implements CommandLineRunner {
 
-	private JsonProcessor jsonProcessor;
-
-	private CsvProcessor csvProcessor;
-
-	private ObjectMapper mapper;
+	private Runner runner;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrdersApplication.class, args);
@@ -39,39 +20,6 @@ public class OrdersApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
-		Checker.checkSupportedFormats(args);
-
-		Stream<String> csvs = Arrays.stream(args).filter(arg -> arg.endsWith(".csv"));
-		Stream<String> jsons = Arrays.stream(args).filter(arg -> arg.endsWith(".json"));
-
-		CompletableFuture<List<Order>> jsonOrders = jsonProcessor.parseToObjectsFromStream(jsons);
-		CompletableFuture<List<Order>> csvOrders = csvProcessor.parseToObjectsFromStream(csvs);
-		CompletableFuture.allOf(jsonOrders, csvOrders).join();
-		List<Order> orders = null;
-		try {
-			orders = jsonOrders.get();
-		} catch (InterruptedException e) {
-			log.error("JsonProcessor has been Interrupted");
-			System.exit(1);
-		} catch (ExecutionException e) {
-			log.error("JsonProcessor threw exception - {}", e::getCause);
-			System.exit(1);
-		}
-
-		try {
-			orders.addAll(csvOrders.get());
-		} catch (InterruptedException e) {
-			log.error("CsvProcessor has been Interrupted");
-		} catch (ExecutionException e) {
-			log.error("CsvProcessor threw exception - {}", e::getCause);
-		}
-		orders.forEach(order -> {
-			try {
-				System.out.println(mapper.writeValueAsString(order));
-			} catch (JsonProcessingException e) {
-				log.error("Somehow we couldn't map Order from file {} on line {} back to json - {}",
-						order::getFilename, order::getId, e::getCause);
-			}
-		});
+		runner.runProcessing(args);
 	}
 }
---
+++ src/main/java/org/parser/orders/runner/Runner.java
@@ -0,0 +1,65 @@
+package org.parser.orders.runner;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.parser.orders.model.Order;
+import org.parser.orders.processor.Processor;
+import org.parser.orders.utils.Checker;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Stream;
+
+@Log4j2
+@AllArgsConstructor
+@Component
+public class Runner {
+
+	private Processor jsonProcessor;
+
+	private Processor csvProcessor;
+
+	private ObjectMapper mapper;
+
+	public void runProcessing(String[] args) {
+		Checker.checkSupportedFormats(args);
+
+		Stream<String> csvs = Arrays.stream(args).filter(arg -> arg.endsWith(".csv"));
+		Stream<String> jsons = Arrays.stream(args).filter(arg -> arg.endsWith(".json"));
+
+		CompletableFuture<List<Order>> jsonOrders = jsonProcessor.parseToObjectsFromStream(jsons);
+		CompletableFuture<List<Order>> csvOrders = csvProcessor.parseToObjectsFromStream(csvs);
+		CompletableFuture.allOf(jsonOrders, csvOrders).join();
+		List<Order> orders = null;
+		try {
+			orders = jsonOrders.get();
+		} catch (InterruptedException e) {
+			log.error("JsonProcessor has been Interrupted");
+			System.exit(1);
+		} catch (ExecutionException e) {
+			log.error("JsonProcessor threw exception - {}", e::getCause);
+			System.exit(1);
+		}
+
+		try {
+			orders.addAll(csvOrders.get());
+		} catch (InterruptedException e) {
+			log.error("CsvProcessor has been Interrupted");
+		} catch (ExecutionException e) {
+			log.error("CsvProcessor threw exception - {}", e::getCause);
+		}
+		orders.forEach(order -> {
+			try {
+				System.out.println(mapper.writeValueAsString(order));
+			} catch (JsonProcessingException e) {
+				log.error("Somehow we couldn't map Order from file {} on line {} back to json - {}",
+						order::getFilename, order::getId, e::getCause);
+			}
+		});
+	}
+}
